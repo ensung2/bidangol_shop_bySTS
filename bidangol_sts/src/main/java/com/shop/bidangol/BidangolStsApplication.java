a92de3e90ddb2340e3b1ba60bf2dd23b7fbfc9b8
ensung2 <iieunsung@gmail.com>
2024-04-04 17:02:52 +0900

    240404 회원가입 비밀번호 암호화(해시,BCryptPasswordEncoder) 설정

--- bidangol_sts/src/main/java/com/shop/bidangol/BidangolStsApplication.java
+++ bidangol_sts/src/main/java/com/shop/bidangol/BidangolStsApplication.java
@@ -2,8 +2,9 @@ package com.shop.bidangol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BidangolStsApplication {
 
 	public static void main(String[] args) {
--- bidangol_sts/src/main/java/com/shop/bidangol/controller/LoginController.java
+++ bidangol_sts/src/main/java/com/shop/bidangol/controller/LoginController.java
@@ -1,71 +1,34 @@
 package com.shop.bidangol.controller;
 
-import java.io.UnsupportedEncodingException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.shop.bidangol.service.UserService;
 import com.shop.bidangol.vo.UserVO;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Controller
 public class LoginController {
-
-	private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private BCryptPasswordEncoder passEncoder;
+
+	// 로그인 페이지 진입
 	@GetMapping("/login")
-	public String loginPage(HttpServletRequest request) {
-		Cookie[] cookies = request.getCookies();
-		String id = null;
-
-		if (cookies != null) {
-			for (Cookie c : cookies) {
-				if ("id".equals(c.getName())) {
-					id = c.getValue();
-					break;
-				}
-			}
-		}
-		if (id != null) {
-			request.setAttribute("id", id);
-		}
-
+	public String loginPage() {
 		return "login/loginPage";
 	}
 
-	// 로그아웃
-	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-
-		session.invalidate(); // 세션종료
-
-		return "redirect:/Index";
-	}
-
-	@GetMapping("/signUp/success")
-	public String successPage(@RequestParam String id, Model model) {
-
-		model.addAttribute("userVO", new UserVO());
-		return "login/signSuccess";
-	}
-
+	// 회원가입 페이지 진입
 	@GetMapping("/signUp")
 	public String signUpPage() {
 		return "login/signUp";
@@ -74,8 +37,31 @@ public class LoginController {
 	// 회원가입
 	@PostMapping("/signUp/joinUser")
 	public String joinUser(@ModelAttribute UserVO userVO) {
+		
+		  String inputPass = userVO.getPassword();
+		  String pass = passEncoder.encode(inputPass); userVO.setPassword(pass);
+		 
 		userService.joinUser(userVO);
 		return "Index";
+	}
+
+	// 회원가입 성공시 페이지
+	@GetMapping("/signUp/success")
+	public String successPage(@RequestParam String id, Model model) {
+		model.addAttribute("userVO", new UserVO());
+		return "login/signSuccess";
+	}
+
+	// 정보입력 후 로그인 시도
+	@PostMapping("/login/action")
+	@ResponseBody
+	public String login(@RequestParam("id") String id, @RequestParam("password") String password) {
+		UserVO userVO = userService.loginCheck(id, password);
+		if (id != null) {
+			return "success";
+		} else {
+			return "failure";
+		}
 	}
 
 }
